45e6c7df22aa9907d12a5e64ceb5b343cc2b3b22
Isamilmaher <ismailmaher6977@gmail.com>
2026-03-26 05:51:49 +0200

    Theoretical Questions

--- G_Net_34_OOP04/Program.cs
+++ G_Net_34_OOP04/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace G_Net_34_OOP04
+﻿using System.Collections;
+using System.Numerics;
+using System.Security.Claims;
+using System.Timers;
+using System.Xml.Linq;
+
+namespace G_Net_34_OOP04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01 : Theoretical Questions
+            //        Q1: What is the difference between static binding and dynamic binding? When does each one happen ?
+
+            //Static binding means the method call is resolved at compile time.When in Compile Time 
+            //Dynamic binding means the method call is resolved at runtime based on the actual object.When in Runtime
+
+            //Q2 : What is the difference between method overloading and method overriding?
+
+            //method overloading   Same method name, different parameter lists — within the same class. Resolved at compile time.
+            //method overriding Derived class overrides a virtual method from the base class using the override keyword.
+
+            //Q3: What keywords are used for Method Overriding? What does each one mean ?
+            //virtual:Base Class.
+            //override :Used in the Derived Class.
+            //base:Used inside the Derived Class method.
+
+        #endregion
         }
     }
 }
